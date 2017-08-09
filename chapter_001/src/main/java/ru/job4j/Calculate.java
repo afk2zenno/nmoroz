8148566db02f89c6e64d805286fb5857c515e5ff
afk2zenno <afk2zenno@gmail.com>
2017-08-09 18:31:19 +0300

    task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,6 +1,18 @@
-package ru.job4j;
+﻿package ru.job4j;
+
+/**
+* Class Calculate для вывода словосочетания "Hello World".
+* @author Nikolay Moroz
+* @version 1.0
+* @since 09.08.2017
+*/
 
 public class Calculate {
+   
+   /**
+   * Метод main, с помощью которого выводится "Hello World" в консоль.
+   * @param arg - arg.
+   */
    
     public static void main(String[] args) {
         System.out.println("Hello World");
